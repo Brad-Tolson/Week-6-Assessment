a2cf248fad1e1a85f655b83ee643476c8113fb8f
Brad Tolson <batolson8605@yahoo.com>
2023-03-03 14:41:46 -0500

    first commit

--- functions.test.ts
+++ functions.test.ts
@@ -1,18 +1,18 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    it('return an array', () => {
+    test('return an array', () => {
         const result = shuffleArray([1, 2, 3]);
         expect(Array.isArray(result)).toBe(true);
     });
 
-    it('return an array of the same length as the input', () => {
+    test('return an array of the same length as the input', () => {
         const input = [1, 2, 3, 4, 5];
         const result = shuffleArray(input);
         expect(result.length).toBe(input.length);
     });
 
-    it('return an array with the same items', () => {
+    test('return an array with the same items', () => {
         const input = [1, 2, 3, 4, 5];
         const result = shuffleArray(input);
         input.forEach(item => {
@@ -20,7 +20,7 @@ describe('shuffleArray should', () => {
         });
     });
 
-    it('return an array with shuffled items', () => {
+    test('return an array with shuffled items', () => {
         const input = [1, 2, 3, 4, 5];
         const result = shuffleArray(input);
         expect(result).not.toEqual(input);
